d26192fd8f6ce8dffb774c608d2ec77228bc3eca
Bogdan Poruchnyk <bogomdannyibogdan@example.com>
2026-02-03 20:12:05 +0200

    first commit

---
+++ calculator/include/cpp_2k.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <cmath>
+
+class Calculator {
+    const double a_;
+    const double b_;
+public:
+    Calculator(const double& a, const double& b) : a_(a), b_(b) {}
+
+    const double calculate(const double& x) const {
+        return std::tan(2 * a_ - std::sin(b_ * x)) / (x * std::log(std::abs(2 - x)));
+    }
+
+    const double operator()(const double& x) const { return this->calculate(x); }
+};
---
+++ calculator/src/cpp_2k.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "cpp_2k.hpp" 
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    double a = 3.71, b = 5.22, x = -4.08;
+
+    cout << "Enter a, b, x: ";
+    cin >> a >> b >> x;
+
+    Calculator calculator(a, b);
+    cout << "Result: " << calculator(x) << endl;
+
+    return 0;
+}
