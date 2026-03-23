0cda16b92b335446ec53a943a3f26cebe5ffa602
Bogdan Pruchnik <bogomdannyibogdan@gmail.com>
2026-03-23 11:17:59 +0200

    Initial commit for cpp_2k

---
+++ projects/bits/include/main.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 23.03.2026.
+//
+
+#ifndef CPP_2K_BITS_H
+#define CPP_2K_BITS_H
+
+
+class main {
+};
+
+
+#endif //CPP_2K_BITS_H
---
+++ projects/bits/src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 23.03.2026.
+//
+
+#include "../include/main.hpp"
